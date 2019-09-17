7d862e69ff2073963e78bda21b5ad937dd062457
junngo <myeongjun.ko@gmail.com>
2019-09-17 23:59:27 +0900

    added api call for data

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,28 @@ const header = document.querySelector("#header");
 const fp_from = flatpickr("#from", {defaultDate: "today", minDate: "today"});
 const fp_to = flatpickr("#to", {minDate: "today"});
 
+const searchForm = document.querySelector("#form-search");
+
+
+// Submit Event
+const API_URL = 'https://javascript-basic.appspot.com/searchLocation';
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  let from_date = fp_from.formatDate(new Date(), "Y-m-d");
+  let to_date = fp_to.formatDate(new Date(), "Y-m-d");
+
+  await fetch(API_URL+`?from=${from_date}&to=${to_date}`)
+  .then(response => response.json())
+  .then(json => {
+      console.log(json)
+    }
+  );
+
+}
+
+
 // Scroll Event
 let last_known_scroll_position = 0;
 let ticking = false;
@@ -27,6 +49,8 @@ function init() {
           ticking = true;
         }
       });
+
+      searchForm.addEventListener("submit", handleSubmit);
 }
 
 init()
