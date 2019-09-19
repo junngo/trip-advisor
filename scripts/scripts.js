14578a8dabcc1047a8a1e4a41c689dc02c3c1e43
junngo <myeongjun.ko@gmail.com>
2019-09-19 15:51:21 +0900

    added main page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,14 @@
 const header = document.querySelector("#header");
+
 const fp_from = flatpickr("#from", {defaultDate: "today", minDate: "today"});
 const fp_to = flatpickr("#to", {minDate: "today"});
 
 const searchForm = document.querySelector("#form-search");
+
+const item_bg = document.querySelector("#list-bg"),
+      item_list = item_bg.querySelector("#list-panel");
+
+const template = document.querySelector("#list-item-template");
 
 
 // Submit Event
@@ -10,6 +16,7 @@ const API_URL = 'https://javascript-basic.appspot.com/searchLocation';
 
 async function handleSubmit(event) {
   event.preventDefault();
+  while(item_list.firstChild) item_list.removeChild(item_list.firstChild);
 
   let from_full = fp_from.selectedDates[0];
   let to_full = fp_to.selectedDates[0];
@@ -25,7 +32,18 @@ async function handleSubmit(event) {
     if (request.status >= 200 && request.status < 400) {
       // Success!
       var data = JSON.parse(request.responseText);
-      console.log(data);
+      console.log(data[0]);
+
+      item_bg.style.display = 'inline-block';
+      for (let i = 0; i < data.length; i++) {
+        template_node = template.cloneNode(true);
+ 
+        template_node.querySelector(".list-item-image").src = data[i].titleImageUrl;
+        template_node.querySelector(".list-item-name").innerHTML = data[i].name;
+        template_node.querySelector(".list-item-city-name").innerHTML = data[i].cityName;
+        
+        item_list.appendChild(template_node);
+      }
 
     } else {
       // We reached our target server, but it returned an error
@@ -54,21 +72,33 @@ function scrollEventCall(scroll_pos) {
   }
 }
 
-function init() {
-    window.addEventListener('scroll', function(e) {
-        last_known_scroll_position = window.scrollY;
-      
-        if (!ticking) {
-          window.requestAnimationFrame(function() {
-            scrollEventCall(last_known_scroll_position);
-            ticking = false;
-          });
-      
-          ticking = true;
-        }
-      });
+// List Click Event -> Move Page
+function handleListClick(event) {
+  event.preventDefault();
+  console.log("list click");
 
-      searchForm.addEventListener("submit", handleSubmit);
+  // let url = 'detail.html?id='+data.id;
+  // window.location = url;
+}
+
+function init() {
+  template.remove();
+
+  window.addEventListener('scroll', function(e) {
+      last_known_scroll_position = window.scrollY;
+    
+      if (!ticking) {
+        window.requestAnimationFrame(function() {
+          scrollEventCall(last_known_scroll_position);
+          ticking = false;
+        });
+    
+        ticking = true;
+      }
+  });
+
+  searchForm.addEventListener("submit", handleSubmit);
+  template.addEventListener("click", handleListClick);
 }
 
 init()
